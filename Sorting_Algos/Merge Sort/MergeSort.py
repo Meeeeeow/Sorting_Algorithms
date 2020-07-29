20325a168e758a7b0f8f7f5fdf27fe919375a3bd
Plunderer <kazutokun467@gmail.com>
2020-07-30 03:21:32 +0600

    Updated #0th July with an update

--- Sorting_Algos/Merge Sort/MergeSort.py
+++ Sorting_Algos/Merge Sort/MergeSort.py
@@ -41,3 +41,41 @@ printList(arr)
 mergeSort(arr)
 print("Sorted array is: ", end="\n")
 printList(arr)
+
+
+#Solution 2
+def merge_sort(values):
+    if len(values) > 1:
+        m = len(values) // 2
+        left = values[:m]
+        right = values[m:]
+        merge_sort(left)
+        merge_sort(right)
+
+        values = []
+
+        while len(left) > 0 and len(right) > 0:
+            if left[0] < right[0]:
+                values.append(left[0])
+                left.pop(0)
+            else:
+                values.append(right[0])
+                right.pop(0)
+
+        for i in left:
+            values.append(i)
+        for i in right:
+            values.append(i)
+
+    return values
+
+
+a = [12, 11, 13, 5, 6, 7]
+print("Given array is")
+print(a)
+
+my_list = merge_sort(a)
+
+# Print output
+print("Sorted array is : ")
+print(my_list) 
